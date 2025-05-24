79c53bde03c9d75783664505eb4834b7f968708e
williamxiewz <williamxiewz@gmail.com>
2025-05-24 15:00:47 +0800

    feat:api 接口

--- clwy-api/app.js
+++ clwy-api/app.js
@@ -5,6 +5,9 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+// 后台路由文件
+const adminArticlesRouter = require('./routes/admin/articles');
+
 
 const app = express();
 
@@ -16,5 +19,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// 后台路由配置
+app.use('/admin/articles', adminArticlesRouter);
+
+
 
 module.exports = app;
---
+++ clwy-api/routes/admin/articles.js
@@ -0,0 +1,161 @@
+const express = require('express');
+const router = express.Router();
+const { Article } = require('../../models');
+
+/**
+ * 查询文章列表
+ * GET /admin/articles
+ */
+router.get('/', async function (req, res) {
+    try {
+        // 定义查询条件
+        const condition = {
+            order: [['id', 'DESC']]
+        };
+
+        // 查询数据
+        const articles = await Article.findAll(condition);
+
+        // 返回查询结果
+        res.json({
+            status: true,
+            message: '查询文章列表成功。',
+            data: {
+                articles
+            }
+        });
+    } catch (error) {
+        // 返回错误信息
+        res.status(500).json({
+            status: false,
+            message: '查询文章列表失败。',
+            errors: [error.message]
+        });
+    }
+});
+
+
+/**
+ * 查询文章详情
+ * GET /admin/articles/:id
+ */
+router.get('/:id', async function (req, res) {
+    try {
+        // 获取文章 ID
+        const { id } = req.params;
+
+        // 查询文章
+        const article = await Article.findByPk(id);
+
+        if (article) {
+            res.json({
+                status: true,
+                message: '查询文章成功。',
+                data: article
+            });
+        } else {
+            res.status(404).json({
+                status: false,
+                message: '文章未找到。',
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            status: false,
+            message: '查询文章失败。',
+            errors: [error.message]
+        });
+    }
+});
+
+
+/**
+ * 创建文章
+ * POST /admin/articles
+ */
+router.post('/', async function (req, res) {
+    try {
+        // 使用 req.body 获取到用户通过 POST 提交的数据，然后创建文章
+        const article = await Article.create(req.body);
+
+        res.status(201).json({
+            status: true,
+            message: '创建文章成功。',
+            data: article
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: false,
+            message: '创建文章失败。',
+            errors: [error.message]
+        });
+    }
+});
+
+
+
+router.delete('/:id', async function (req, res) {
+    try {
+        // 获取文章 ID
+        const { id } = req.params;
+
+        // 查询文章
+        const article = await Article.findByPk(id);
+
+        if (article) {
+            // 删除文章
+            await article.destroy();
+
+            res.json({
+                status: true,
+                message: '删除文章成功。'
+            });
+        } else {
+            res.status(404).json({
+                status: false,
+                message: '文章未找到。',
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            status: false,
+            message: '删除文章失败。',
+            errors: [error.message]
+        });
+    }
+});
+
+/**
+ * 更新文章
+ * PUT /admin/articles/:id
+ */
+router.put('/:id', async function (req, res) {
+    try {
+        const { id } = req.params;
+        const article = await Article.findByPk(id);
+
+        if (article) {
+            await article.update(req.body);
+
+            res.json({
+                status: true,
+                message: '更新文章成功。',
+                data: article
+            });
+        } else {
+            res.status(404).json({
+                status: false,
+                message: '文章未找到。',
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            status: false,
+            message: '更新文章失败。',
+            errors: [error.message]
+        });
+    }
+});
+
+
+module.exports = router;
